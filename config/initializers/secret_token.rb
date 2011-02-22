ba9079ee20334089e60fc863d5076881b2bcee14
agarg <anshul.garg@gmail.com>
2011-02-21 18:55:54 -0600

    Setting up Blogger App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = '16019b7f2cf0ff4d572ed0891452bb30bb4eb978ac3f2a3690ff8686f38cb4231c5480a5198385e4cbd057f6d34310c0b357251e2295893b33da3b835c6500ec'
