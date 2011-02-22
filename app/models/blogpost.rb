2ab85aefa8ecc2dc164ba17b6d0e608f56cce1d0
agarg <anshul.garg@gmail.com>
2011-02-21 19:28:40 -0600

    Added Blopost model

--- app/models/blogpost.rb
+++ app/models/blogpost.rb
@@ -1,2 +1,7 @@
 class Blogpost < ActiveRecord::Base
+  attr_accessible :title, :content
+  validates :title, :presence => true,
+                    :uniqueness => true
+  validates :content, :presence => true
+  
 end
--- spec/models/blogpost_spec.rb
+++ spec/models/blogpost_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe Blogpost do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @attr = { :title => "Dummy title", :content => "dummy content" }
+  end
+
+  it "should create a new instance given valid attributes" do
+    Blogpost.create!(@attr)
+  end
+
+  it "should require a title" do
+    no_title_blogpost = Blogpost.new(@attr.merge(:title => ""))
+    no_title_blogpost.should_not be_valid
+  end
+  
+  it "should require content" do
+    no_content_blogpost = Blogpost.new(@attr.merge(:content => ""))
+    no_content_blogpost.should_not be_valid
+  end
+  
+  it "should require title to be unique" do
+    unique_title_blogpost = Blogpost.create(@attr.merge(:title => "Unique title"))
+    non_unique_title_blogpost = Blogpost.new(@attr.merge(:title => "Unique title"))
+    non_unique_title_blogpost.should_not be_valid
+  end
 end
