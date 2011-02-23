02c04a65e6097830a893c2b2c69749ba5b34790e
agarg <anshul.garg@gmail.com>
2011-02-23 01:01:47 -0600

    Fixed landing page

--- config/routes.rb
+++ config/routes.rb
@@ -59,4 +59,5 @@ Blogger::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+  root :to => "blogposts#index"
 end
